b241df1b25de5eab963669a9198704d6eb6f038b
David Beck <beckx400@d.umn.edu>
2015-09-21 09:39:25 -0500

    Added a rating not available feature

--- script.js
+++ script.js
@@ -1,39 +1,45 @@
 $(document).ready(function() {
 
-		function searchCallback(results) {
-			console.log(results);
-			var errorMessage = "<div><h3>Sorry! Your search wasn't found...</h3></div>"
-			var metaScore = "";
 
-			if(parseInt(results.Metascore)>= 60){
-				metaScore = "<p class='watch'>You should watch this movie!</p>";
-			} else {
-				metaScore = "<p class='watch red' >This movie isn't worth your time...</p>";
-			}
+	function searchCallback(results) {
+		console.log(results);
+		var errorMessage = "<div><h3>Sorry! Your search wasn't found...</h3></div>"
+		var metaScore = "";
 
-			var appendMessage = "<div><h3>" + results.Title + "</h3><p>" + results.Plot + "</p><br>" + metaScore + "</div>";
+		if(parseInt(results.Metascore)>= 60){
+			metaScore = "<p class='watch blue'>You should watch this movie!</p>";
+		} else if(results.Metascore == "N/A") {
+			metaScore = "<p class='watch'>No rating available, so give it a shot!</p>"
 
-			if(results.Title === undefined){
-				$('.movie-info').html(errorMessage);
-			} else {
-		    $('.movie-info').html(appendMessage);
-			}
+		} else {
+			metaScore = "<p class='watch red' >This movie isn't worth your time...</p>";
 		}
 
-		$("form").on("submit", function(event){
+		var appendMessage = "<div><h3>" + results.Title + "</h3><p>" + results.Plot + "</p><br>" + metaScore + "</div>";
+
+		if(results.Title === undefined){
+			$('.movie-info').html(errorMessage);
+		} else {
+	    	$('.movie-info').html(appendMessage);
+		}
+	}
+
+	$("form").on("submit", function(event){
 		event.preventDefault();
 
 		var $inputs = $(".myInputs input");
 		var values = {};
 
-			$inputs.each(function(){
-				values[this.name] = $(this).val();
-			});
+		$inputs.each(function(){
+			values[this.name] = $(this).val();
+		});
+
 		search(values.movie);
+
 		$(".myInputs input[type=text]").val('');
 		});
 
-		function search(query){
+	function search(query){
 
 		$.ajax ({
 		    type: 'GET',
@@ -46,7 +52,3 @@ $(document).ready(function() {
 		});
 	}
 });
-
-
-
-
